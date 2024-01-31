c041b57f8a1dfc46c00807aca2756191a880e223
Rahees9983 <38326225+Rahees9983@users.noreply.github.com>
2024-01-31 22:15:24 +0530

    aaa

--- scripts/print_inputs.sh
+++ scripts/print_inputs.sh
@@ -3,7 +3,8 @@ set -e
 
 my_func() {
     echo "Deleting image ${1}:${2}"
-    echo "Using ACL_NAME --->> $ACL_NAME"
+    echo "Using ACL_NAME --->> $ACL_NAAM"
 }
 
 my_func $IMAGE_NAAM $IMAGE_TAG
+echo "ACL_NAAM --> $ACL_NAAM"
